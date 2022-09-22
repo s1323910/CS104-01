0e43d516f8d0ba7555f6dddd9830695fb6687a85
s1323910 <114018362+s1323910@users.noreply.github.com>
2022-09-22 12:09:08 -0400

    This is version 2 of my hello world program

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,8 @@
-# This is Version 1 of my hello world program
-print("Hello my name is insert your Emma") 
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================ RESTART: /Users/EmmaGreen/Desktop/helloWorld.py ===============
+Hello my name is insert your Emma
+>>> 
+================ RESTART: /Users/EmmaGreen/Desktop/helloWorld.py ===============
+Hello my name is insert your Emma Green
